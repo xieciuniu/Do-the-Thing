43001859f1e19bbd9cf77f54f8cc70e65139e695
hubertwojtowicz <h.wojtowicz44@gmail.com>
2024-06-06 08:24:56 +0200

    Initial Commit

---
+++ Do the Thing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Do the Thing
+//
+//  Created by Hubert Wojtowicz on 06/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Do the Thing")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Do the Thing/Do_the_ThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Do_the_ThingApp.swift
+//  Do the Thing
+//
+//  Created by Hubert Wojtowicz on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Do_the_ThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
